1392e8342fde01c26d36876ac33e7a41d213cd6b
Deepak Bansode <deepak.bansode@frontdoorhome.com>
2020-09-04 23:48:28 +0530

    Updates

---
+++ precache-manifest.90fd0bb86bd8839952eea32889535edb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524451e0d4d2e03ccb8254f35693f14c",
+    "url": "/truth-or-lie-game/index.html"
+  },
+  {
+    "revision": "14f9abdf29ce723888f1",
+    "url": "/truth-or-lie-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f60f1888fd6811f299ad",
+    "url": "/truth-or-lie-game/static/js/2.7bb990ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/truth-or-lie-game/static/js/2.7bb990ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14f9abdf29ce723888f1",
+    "url": "/truth-or-lie-game/static/js/main.890fbb16.chunk.js"
+  },
+  {
+    "revision": "1a749c8dc388a5c0ed47",
+    "url": "/truth-or-lie-game/static/js/runtime-main.b8d13418.js"
+  }
+]);
