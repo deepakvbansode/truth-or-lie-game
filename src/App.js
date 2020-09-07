95005e7ec3bcd7fcf4b8afb5215715d13ed5ebb8
Deepak Bansode <deepak.bansode@frontdoorhome.com>
2020-09-07 14:36:33 +0530

    final release

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import "./App.css";
-
+import {Alert} from "reactstrap";
 import { Home } from "./pages/Home";
 import { Admin } from "./pages/Admin";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
- 
 
 function App() {
-  return <Router>
-  <Switch>
-    <Route exact path="/truth-or-lie-game" component={Home}></Route>
-    <Route path="/truth-or-lie-game/admin" component={Admin}></Route>
-  </Switch>
-</Router>
+  return (
+    <div>
+      <Alert color="primary">
+
+      <h1>Welcome to Frontdoor's Cross Functional Team Game zone! </h1>
+      </Alert>
+    <Router>
+      <Switch>
+        <Route exact path="/truth-or-lie-game" component={Home}></Route>
+        <Route path="/truth-or-lie-game/admin" component={Admin}></Route>
+      </Switch>
+    </Router>
+    </div>
+  );
 }
 
 export default App;
